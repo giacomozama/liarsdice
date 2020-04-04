4f4312a2540cce2bf38932e9a96ed3fa5b511732
Alessandro Fusco <alessandro.fusco3@studio.unibo.it>
2020-04-04 12:34:12 -0700

    Updates

---
+++ precache-manifest.3ac8904fb749c420f5d4313a82ef0244.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dc2e62af6933a26f96f47f404b122bc",
+    "url": "/giacomozama.github.io/liarsdice/index.html"
+  },
+  {
+    "revision": "34eb86f372732b10c01a",
+    "url": "/giacomozama.github.io/liarsdice/static/css/main.1a3e36fb.chunk.css"
+  },
+  {
+    "revision": "ff9ee59bdbe3f5c27cb0",
+    "url": "/giacomozama.github.io/liarsdice/static/js/2.6bee654b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/giacomozama.github.io/liarsdice/static/js/2.6bee654b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34eb86f372732b10c01a",
+    "url": "/giacomozama.github.io/liarsdice/static/js/main.d3e4c0c2.chunk.js"
+  },
+  {
+    "revision": "4f3e9da6f6b8839a816a",
+    "url": "/giacomozama.github.io/liarsdice/static/js/runtime-main.82ffb7d6.js"
+  }
+]);
