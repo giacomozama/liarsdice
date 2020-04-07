f06ce75b6116774ddbf43ea7ac9f696c451b7a3a
Alessandro Fusco <alessandro.fusco3@studio.unibo.it>
2020-04-07 14:32:52 +0200

    Updates

--- precache-manifest.3bccbd686b266a8f34a0418e91682247.js
+++ precache-manifest.3bccbd686b266a8f34a0418e91682247.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f9fe3d3d22dd8afb4e066b9c20c4db37",
+    "revision": "46b8213adb2a5e885d7c95201aeba6af",
     "url": "/liarsdice/index.html"
   },
   {
-    "revision": "552f807483baf38907bb",
+    "revision": "834d31beb6430408fb6d",
     "url": "/liarsdice/static/css/main.b9d489c5.chunk.css"
   },
   {
-    "revision": "831fc8ce4d09a3a53799",
-    "url": "/liarsdice/static/js/2.e8408726.chunk.js"
+    "revision": "4ac0c0c037896bf9fe1e",
+    "url": "/liarsdice/static/js/2.d6408ace.chunk.js"
   },
   {
     "revision": "f65b45f562155f0fa2653b346e3552a6",
-    "url": "/liarsdice/static/js/2.e8408726.chunk.js.LICENSE.txt"
+    "url": "/liarsdice/static/js/2.d6408ace.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "552f807483baf38907bb",
-    "url": "/liarsdice/static/js/main.57445d94.chunk.js"
+    "revision": "834d31beb6430408fb6d",
+    "url": "/liarsdice/static/js/main.3055e139.chunk.js"
   },
   {
     "revision": "7f114b25fff12da42500",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/liarsdice/precache-manifest.cf4168bfc1836e70a87771a95e988e32.js"
+  "/liarsdice/precache-manifest.3bccbd686b266a8f34a0418e91682247.js"
 );
 
 self.addEventListener('message', (event) => {
